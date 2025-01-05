ecb7e8e8a29a619b134daeac63abb66df7584f63
Kaspar <kspr9@pm.me>
2025-01-05 17:54:27 +0200

    Strange. BeaconWallet would not accept networktype.ghostnet. after digging in docs decided to change BeaconWallet to DAppClient, and that solved the network issue. Not sure if DAppClient is same as BeaconWallet

--- src/lib/WalletConnectorBeacon.svelte
+++ src/lib/WalletConnectorBeacon.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { TezosToolkit } from '@taquito/taquito';
     import { BeaconWallet } from '@taquito/beacon-wallet';
-    import { BeaconEvent } from '@airgap/beacon-dapp';
+    import { DAppClient, type DAppClientOptions, NetworkType } from '@airgap/beacon-dapp';
     import { onMount } from 'svelte';
 
     export const walletState = $state({
@@ -9,25 +9,27 @@
         isConnected: false
     });
 
+    const options: DAppClientOptions = {
+        name: 'Tokensharebeacon',
+        iconUrl: 'https://taquito.io/img/favicon.svg',
+        network: { type: NetworkType.GHOSTNET },
+        enableMetrics: true,
+    };
+
     let userAddress: string;
 
     async function walletInit() {
         const Tezos = new TezosToolkit('https://ghostnet.smartpy.io');
-        const wallet: BeaconWallet = new BeaconWallet({
-            name: 'Tokensharebeacon'
-        });
-        Tezos.setWalletProvider(wallet);
-        wallet.client.subscribeToEvent(BeaconEvent.ACTIVE_ACCOUNT_SET, (account) => {
-            // An active account has been set, update the dApp UI
-            console.log(`${BeaconEvent.ACTIVE_ACCOUNT_SET} triggered: `, account);
-        });
+        const wallet: DAppClient = new DAppClient(options);
         try {
             console.log("Requesting permissions...");
-            const permissions = await wallet.client.requestPermissions();
+            const permissions = await wallet.requestPermissions();
             console.log("Got permissions:", permissions.address);
         } catch (error) {
             console.error("Got error:", error);
         }
+        //Tezos.setWalletProvider(wallet);
+        
 
     };
     
